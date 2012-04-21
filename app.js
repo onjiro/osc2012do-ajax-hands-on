fc5e8706c56dd2e1ff6433941b0489e8217c664a
onjiro <onjiro.mohyahya@gmail.com>
2012-04-22 00:54:06 +0900

    app.js にて view_samples ディレクトリをポート3000で公開する

---
+++ app.js
@@ -0,0 +1,7 @@
+express = require('express');
+app = express.createServer();
+
+app.use(express.static(__dirname + '/view_sample'));
+
+app.listen(3000);
+console.log('server start at localhost:3000');
