e4ac00fe529815968cb548b476127626e143dba3
onjiro <onjiro.mohyahya@gmail.com>
2012-04-22 09:19:18 +0900

    前のコミットに伴いインデントを適切に設定

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 (function(app, port) {
-app.use(express.static(__dirname + '/view_sample'));
+    app.use(express.static(__dirname + '/view_sample'));
 
-app.listen(port);
-console.log('server start at localhost:' + port);
+    app.listen(port);
+    console.log('server start at localhost:' + port);
 })(express.createServer(), 3000);
