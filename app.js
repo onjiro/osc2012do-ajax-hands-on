6f352302429dbb097a0393b3ee56891f42240dd6
onjiro <onjiro.mohyahya@gmail.com>
2012-05-13 01:39:25 +0900

    フロント側を用意

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var mongo_builder = require('./lib/mongo_builder');
 
-(function(app, port) {
+var start = function(app, db, port) {
     app.use(express.static(__dirname + '/view_sample'));
+    app.use(express.bodyParser());
+    
+    var reservationUrl = '/reservations/';
+    app.get(reservationUrl + ':date.:roomId?.:division?', function(req, res){
+        var reservations = [];
+        res.send(reservations);
+    });
+    app.post(reservationUrl + ':date.:roomId.:division', function(req, res){
+        // TODO res.send(err);
+    });
+    app.del(reservationUrl + ':date.:roomId.:division', function(req, res){
+        // TODO res.send(err)
+    });
 
     app.listen(port);
     console.log('server start at localhost:' + port);
-})(express.createServer(), 3000);
+};
+
+mongo_builder.ready('room-reservations', function(db){
+    start(express.createServer(), db, 3000);
+});
